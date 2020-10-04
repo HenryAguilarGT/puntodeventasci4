ddcbb7d2a30a844237cd446d973cbf3826df5915
Henry Belmonte <henryenlineagt@gmail.com>
2020-10-03 21:17:09 -0600

    corrigiendo carcter suelto

--- app/Views/categorias/nuevo.php
+++ app/Views/categorias/nuevo.php
@@ -8,7 +8,7 @@
                           <div class="col-6 col-sm-6 py-3">
                               <label>Nombre</label>
                               <input type="form-control" id="nombre" name="nombre" type="text" autofocus required>
-                          </div>>
+                          </div>
                       </div>
                   </div>
                   <a href="<?php echo base_url(); ?>/categorias" class="btn btn-primary">Regresar</a>
